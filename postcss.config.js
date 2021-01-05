702e71f24f15bfd8509685c03d0b39450bdb2668
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-05 15:30:20 -0300

    Temp. style fix

--- postcss.config.js
+++ postcss.config.js
@@ -13,18 +13,18 @@ let environment = {
   ]
 }
 
-if (process.env.RAILS_ENV === 'production') {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-        './app/javascript/**/*.elm'
-      ],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
+// if (process.env.RAILS_ENV === 'production') {
+//   environment.plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: [
+//         './app/**/*.erb',
+//         './app/helpers/**/*.rb',
+//         './app/javascript/**/*.js',
+//         './app/javascript/**/*.elm'
+//       ],
+//       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//     })
+//   )
+// }
 
 module.exports = environment;
