b3894ca8eb15f5ca453c0991898e3b1b4da08833
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-23 01:26:56 -0300

    Add try catch to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,10 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue
+    end
   end
 
   def user_signed_in?
