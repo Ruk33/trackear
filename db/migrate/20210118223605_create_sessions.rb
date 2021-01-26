2a5d4b0d48a14101524216c2afbf524e1fa8cf96
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-26 01:08:05 -0300

    Create sessions table only if it does not exists

--- db/migrate/20210118223605_create_sessions.rb
+++ db/migrate/20210118223605_create_sessions.rb
@@ -1,5 +1,7 @@
 class CreateSessions < ActiveRecord::Migration[6.0]
   def change
+    return if table_exists? :sessions
+
     create_table :sessions do |t|
       t.string :token
       t.references :user, null: false, foreign_key: true
